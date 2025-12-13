bdcb3c1093629d147bc227b4018d8c7e9bc9e239
sunny kumar <72156768+sunnykumar7645@users.noreply.github.com>
2025-12-13 13:23:40 +0000

    global exceptions handling

---
+++ jwtsecuritydemo/src/main/java/com/jwt/jwtsecuritydemo/error/ApiError.java
@@ -0,0 +1,26 @@
+package com.jwt.jwtsecuritydemo.error;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Data;
+
+@Data
+public class ApiError {
+    
+    private LocalDateTime timeStamp;
+    private String error;
+    private HttpStatus statusCode;
+
+    public ApiError(){
+        this.timeStamp = LocalDateTime.now();
+    }
+
+    public ApiError(String error, HttpStatus statusCode){
+        this();
+        this.error = error;
+        this.statusCode = statusCode;
+    }
+}
--- jwtsecuritydemo/src/main/java/com/jwt/jwtsecuritydemo/security/JwtAuthFilter.java
+++ jwtsecuritydemo/src/main/java/com/jwt/jwtsecuritydemo/security/JwtAuthFilter.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import com.jwt.jwtsecuritydemo.model.User;
 import com.jwt.jwtsecuritydemo.repository.UserRepository;
@@ -25,29 +26,35 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
     private final UserRepository userRepository;
     private final AuthUtil authUtil;
+    private final HandlerExceptionResolver handlerExceptionResolver;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)throws ServletException, IOException {
 
-        log.info("Incoming request: {}" + request.getRequestURI());
+        try{
+            log.info("Incoming request: {}" + request.getRequestURI());
 
-        final String requestToken = request.getHeader("Authorization");
-        if(requestToken == null || !requestToken.startsWith("Bearer ")){
+            final String requestToken = request.getHeader("Authorization");
+            if(requestToken == null || !requestToken.startsWith("Bearer ")){
+                filterChain.doFilter(request, response);
+                return;
+            }
+            
+            String token = requestToken.split("Bearer ")[1];
+            String username = authUtil.getUsernameFromToken(token);
+
+            if(username != null && SecurityContextHolder.getContext().getAuthentication() == null ){
+                User user = userRepository.findByUsername(username).orElseThrow();
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+
+            }
             filterChain.doFilter(request, response);
-            return;
-        }
-        
-        String token = requestToken.split("Bearer ")[1];
-        String username = authUtil.getUsernameFromToken(token);
 
-        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null ){
-            User user = userRepository.findByUsername(username).orElseThrow();
-            UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+        } catch(Exception ex){
+            handlerExceptionResolver.resolveException(request, response, null, ex);
 
         }
-        filterChain.doFilter(request, response);
 
         }
     
